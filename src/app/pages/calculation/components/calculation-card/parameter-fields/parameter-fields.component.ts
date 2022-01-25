a55e0e26a74c89f32aa76596f29dc5365f5f365f
jkalata <kalkuba95@gmail.com>
2022-01-25 19:56:24 +0100

    parameter fields local args variable removed,

--- src/app/pages/calculation/components/calculation-card/parameter-fields/parameter-fields.component.ts
+++ src/app/pages/calculation/components/calculation-card/parameter-fields/parameter-fields.component.ts
@@ -1,23 +1,19 @@
 import { Figure, FigureParamsUnion } from './../../../figure/figure.interfaces';
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-parameter-fields',
   templateUrl: './parameter-fields.component.html',
   styleUrls: ['./parameter-fields.component.scss'],
 })
-export class ParameterFieldsComponent implements OnInit {
+export class ParameterFieldsComponent {
   @Input() figure: Figure = {} as Figure;
-  @Output() parameterChange = new EventEmitter();
-  args!: FigureParamsUnion;
-
-  ngOnInit() {
-    this.args = Object.assign(this.figure.args);
-  }
+  @Output() parameterChange: EventEmitter<FigureParamsUnion> =
+    new EventEmitter();
 
   emitParameterChange(event: Event, key: string): void {
     const input = event.target as HTMLInputElement;
-    this.args[key] = Number(input.value);
-    this.parameterChange.emit(this.args);
+    this.figure.args[key] = Number(input.value);
+    this.parameterChange.emit(this.figure.args);
   }
 }
